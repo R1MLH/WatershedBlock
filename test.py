653434825c5660e0d6dedf6e197f8c635d11d784
Erwan <erwan.mailharro@edu.esiee.fr>
2019-02-18 14:06:12 +0100

    algo naif pourri

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import OOC
 import string
+import random
 
 def nearbyV(Graphe, Vertice, cut = []): # retourne les arc voisins
     rList =[]
@@ -10,21 +11,21 @@ def nearbyV(Graphe, Vertice, cut = []): # retourne les arc voisins
         if Vertice in [elt.getX(),elt.getY()]:
             rList.append(elt)
     return rList
-    
+
 def getNeighborVertices(Graphe, Vertice): # retourne les arc voisins
     rList =[] # to be sure that we don't add the same vertice twice
     for elt in Graphe.getEdges():
         if Vertice == elt.getX():
             rList.append(elt.getY()) # elt is a vertice
-        
+
         if Vertice == elt.getY():
             rList.append(elt.getX()) # elt is a vertice
     return rList
-    
+
 def Fminus(Graphe,cut=[]):
     for elt in Graphe.getVertices():
         elt.setW(min([elt.getW() for elt in nearbyV(Graphe,elt,cut)]))
-        
+
 def localFminus(Graphe,Vertice):
     return min([elt.getW() for elt in nearbyV(Graphe,Vertice)])
 
@@ -32,7 +33,7 @@ def isBorder(edge):
     if(edge.getX().getW()<edge.getW() and edge.getY().getW()== edge.getW()) or (edge.getY().getW()<edge.getW() and edge.getX().getW()== edge.getW()):
         return True
     return False
-    
+
 def isInner(edge):
     if(edge.getX().getW()==edge.getW() and edge.getY().getW()== edge.getW()):
         return True
@@ -102,30 +103,37 @@ def BlocThinning(B1,B2,cut):
                     elt.setW(min(localFminus(B2,elt.getX()),localFminus(B1,elt.getY())))
                 change = True
 
+
 def BlocLabelling(B1,B2,cut):
+    #connexeComponent(B1)
+    #connexeComponent(B2)
+    #uniqueLabel([B1,B2])
+    changes = 0
+    for elt in cut:
+        if isInner(elt):
+            previousLabel = elt.getY().getLabel()
+            newLabel =  elt.getX().getLabel()
+            if previousLabel != newLabel
+                changes +=1
+                elt.getY().setLabel(newLabel)
+                    for vertice in B2.getVertices():
+                        if vertice.getLabel() == previousLabel :
+                            vertice.setLabel(newLabel)
+    return changes
+
+def BlocLabelling2(B1,B2,cut):
     connexeComponent(B1)
     connexeComponent(B2)
     uniqueLabel([B1,B2])
-    
+
     for elt in cut:
-        if isInner(elt):
+        if isInner(elt): # arc interne
             previousLabel = elt.getY().getLabel()
             newLabel =  elt.getX().getLabel()
             elt.getY().setLabel(newLabel)
             for vertice in B2.getVertices():
                 if vertice.getLabel() == previousLabel :
                     vertice.setLabel(newLabel)
-                    
-            
-            
-    # change = True
-    # while change == True:
-        # connexeComponent(B1)
-        # connexeComponent(B2)
-        # for elt in cut:
-            # if isInner(elt):
-                # elt.getY().setLabel(elt.getX().getLabel())
-                # #connexecomponent : mettre un paramètre "cut" pour faire le lien entre les deux graphes
 
 
 def uniqueLabel(list_of_blocs):
@@ -137,18 +145,18 @@ def uniqueLabel(list_of_blocs):
         for vertice in bloc.getVertices():
             vertice.setLabel(letter + str(vertice.getLabel()))
         i+=1
-    
+
 def getMinimas(Graph):
     numero = 0
     for x in Graph.getVertices():
         x.setMinLab("UNKNOWN")
-    
-    
+
+
     Y = [] # have been explored
     X = [] #to explore
-    
+
     for x in Graph.getVertices():
-    
+
         if x.getMinLab() == "UNKNOWN":
             Y.clear()
             X = [x] #X.append(x)
@@ -158,11 +166,11 @@ def getMinimas(Graph):
             y = X[0] # y is a vertice
             X.remove(y)
             Y.append(y)
-            
+
             #minVerticeNeighborW = min([elt2.getW() for elt2 in nearbyV(Graph,elt)])
             VerticeNeighbors = getNeighborVertices(Graph,y) #VerticeNeighbors = getNeighborVertices(Graph,x)
             # minVerticeNeighborsW = min([neighbor.getW() for neighbor in VerticeNeighbors])
-            
+
             if ( y.getW() < altitude): # if ( W-(y) < altitude): poids noeuds voisins
                 x.setMinLab("NOT_MIN")
                 x.setMinLab("NOT_MIN")
@@ -196,12 +204,108 @@ def getMinimas(Graph):
                 y.setMinLab(str(0))
 
 
+
+def create_graph(X,Y):
+    # X = imsize.len_x
+    # Y = imsize.len_y
+
+    new_graph = OOC.Graph()
+    n =  X*Y
+
+    for i in range (X):
+        for j in range(Y):
+            new_graph.addVertice(OOC.Vertice(str(i)+str(j)))
+
+
+    for i in range (X):
+        for j in range(1,Y):
+            # print("i :",i,"\n");
+            # print("j :",j,"\n");
+            weight = random.randint(0,5)
+            new_graph.addEdge(OOC.Edge(new_graph.vertices[i*Y+j-1],new_graph.vertices[i*Y+j],weight))
+
+            #vertices = new_graph.getVertices()
+            #[vertice[i] if vertice[i] == ]
+
+
+            if i is not 0:
+                weight = random.randint(0,5)
+                new_graph.addEdge(OOC.Edge(new_graph.vertices[(i-1)*Y+j],new_graph.vertices[i*Y+j],weight))
+                # print("i :",i,"\n");
+                # print("j :",j,"\n");
+            #else :
+
+
+
+
+
+
+        # for k in range(1,Y):
+            # weight = random.randint(0,5)
+            # new_graph.addEdge(OOC.Edge(new_graph.vertices[k-1],new_graph.vertices[k],weight))
+
+    return new_graph
+
+
+
+# def create_graph(X,Y):
+    # # X = imsize.len_x
+    # # Y = imsize.len_y
+
+    # new_graph = OOC.Graph()
+
+    # edges = []
+    # for y in range(Y):
+        # for x in range(X):
+            # pos = x + y * X
+            # eq1 = x + 1 + y * X
+            # if x + 1 < X:
+                # weight = random.randint(0,5)
+                # new_graph.addEdge(OOC.Edge(new_graph.vertices[pos],new_graph.vertices[eq1],weight))
+            # eq2 = x + (y + 1) * X
+            # if y + 1 < Y:
+                # edges.append((pos, eq2))
+                # weight = random.randint(0,5)
+                # new_graph.addEdge(OOC.Edge(new_graph.vertices[pos],new_graph.vertices[eq2],weight))
+     # #weights = [abs(int(i)) for i in np.random.normal(110, 40, len(edges))]
+
+    # #graph1 = Graph(n_vertices=X * Y, edges=edges, weights=weights)
+    # return new_graph
+
+
+
+# def cut_graph(graph,dimensions,nb_x,nb_y):
+    # for nb_x
+    # return cuts
+
+
+
+
+def label_composante_connexe(graphe,taille_bloc):
+    blocs,cuts = cut_graph(graphe,taille_bloc)
+    for i in blocs:
+        connexeComponent(i)
+    uniqueLabel(blocs)
+    changes = 1
+    while(changes !=0):
+        changes = 0
+        for i in range(0,len(blocs)):
+            for j in range(0,len(blocs)):
+                changes += BlocLabelling(blocs[i],blocs[j],cuts[i][j])
+
+
+
+
+
+
+
+
 def main():
     # MGraph = OOC.Graph()
     B1 = OOC.Graph()
     B2 = OOC.Graph()
     cut = []
-    
+
     # Point1= OOC.Vertice("A",0)
     # Point2= OOC.Vertice("B",0)
     # Point3= OOC.Vertice("C",0)
@@ -223,7 +327,7 @@ def main():
     # Edge10 = OOC.Edge(Point6,Point9,1)
     # Edge11 = OOC.Edge(Point7,Point8,2)
     # Edge12 = OOC.Edge(Point8,Point9,1)
-    
+
     # MGraph.addVertice(Point1)
     # MGraph.addVertice(Point2)
     # MGraph.addVertice(Point3)
@@ -233,7 +337,7 @@ def main():
     # MGraph.addVertice(Point7)
     # MGraph.addVertice(Point8)
     # MGraph.addVertice(Point9)
-    
+
     # MGraph.addEdge(Edge1)
     # MGraph.addEdge(Edge2)
     # MGraph.addEdge(Edge3)
@@ -263,7 +367,7 @@ def main():
     PointN= OOC.Vertice("N",0)
     PointO= OOC.Vertice("O",0)
     PointP= OOC.Vertice("P",0)
-    
+
     Edge1 = OOC.Edge(PointA,PointB,0)
     Edge2 = OOC.Edge(PointB,PointC,0)
     Edge3 = OOC.Edge(PointC,PointD,0)
@@ -288,10 +392,10 @@ def main():
     Edge22 = OOC.Edge(PointM,PointN,0)
     Edge23 = OOC.Edge(PointN,PointO,1)
     Edge24 = OOC.Edge(PointO,PointP,3)
-    
-    
-    
-    
+
+
+
+
     B1.addVertice(PointA)
     B1.addVertice(PointB)
     B2.addVertice(PointC)
@@ -308,7 +412,7 @@ def main():
     B1.addVertice(PointN)
     B2.addVertice(PointO)
     B2.addVertice(PointP)
-    
+
     B1.addEdge(Edge1)
     # MGraph.addEdge(Edge2)
     cut.append(Edge2)
@@ -344,19 +448,19 @@ def main():
     # MThinning(MGraph)
     # BThinning(MGraph)
     BlocThinning(B1,B2,cut)
-    
-    
-    
+
+
+
     # connexeComponent(MGraph)
     # B1.affiche()
     # print()
     # B2.affiche()
-    
+
     connexeComponent(B1)
     connexeComponent(B2)
-    
+
     BlocLabelling(B1,B2,cut)
-    
+
     # for vertice in MGraph.getVertices():
         # print(vertice.getName(), " = ", vertice.getMinLab())
     # print()
@@ -369,7 +473,14 @@ def main():
     B2.affiche()
     #print(hasEdge(MGraph,Point2,Point1))
     # MGraph.affiche()
-    pass    
+
+
+    print("teeeeeeeeeeeeeest")
+    testgraph = create_graph(3,4)
+    testgraph.affiche()
+    pass
 
 if __name__ == '__main__':
     main()
+
+# parametre : le graphe et la taille des blocs
